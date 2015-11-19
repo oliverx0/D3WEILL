9f4c636755809711f45c75118ad10352c836c179
Praveen Gupta <pmg83@gmail.com>
2015-11-19 18:02:28 -0500

    HTML home page set up

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
+
+def getFile(file):
+	with open(file) as file_data:
+		data=json.load(file_data)
+	print data
+
+getFile("/Users/praveengupta/Desktop/Wokstudy/sunburst/solr/AbdomenCT/461570125.json")
